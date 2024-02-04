7e3f1566a2d1111b1cb37c5d5600ae44d7b980aa
Himanshu-Sangshetti <himanshusangshetty@gmail.com>
2024-02-04 12:51:27 +0530

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "bucketlisttrackerc920e816": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
